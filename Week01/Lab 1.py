16acdc3e0832c706bc4ba9882363adb5d89de77c
navjotk17 <navjot.mathoda17@gmail.com>
2026-02-22 11:42:55 -0500

    Weekly Lab Completed

--- Week01/Lab 1.py
+++ Week01/Lab 1.py
@@ -1,5 +1,5 @@
 # Sample Coding Questions 01 Week 01
-# [Your First Name] [Your Last Name]
+# [Navjot Kaur] [Mathoda]
 
 # Question 2: Defining an array (list)
 my_array = [1, 4, 7, 9]
--- Week02/Lab 2.py
+++ Week02/Lab 2.py
@@ -2,18 +2,20 @@ import random
 
 choices = ["Rock", "Paper", "Scissors"]
 
-playerChoice = input("Choose a number between the following list: 1-Rock, 2-Paper, 3-Scissors: ")
+playerChoice = input("Enter your choice (1-Rock, 2-Paper, 3-Scissors): ") # 1, 2, 3
 
 playerChoice = int(playerChoice)
 
-# User Input check
 if playerChoice < 1 or playerChoice > 3:
-    print("Error: You should choose a number between 1 and 3!")
+    print("Error: Choice should be between 1 and 3!")
 else:
-    # Develop the game logic through if/elif/else
     computerChoice = random.randint(1, 3)
-    print(playerChoice, computerChoice)
 
+    playerChoiceIndex = choices[playerChoice - 1] # ==> 0, 1, 2
+    computerChoiceIndex = choices[computerChoice - 1]
+    print(playerChoiceIndex,  computerChoiceIndex)
+
+    # Determine the winner logic using if/elif/else
     if playerChoice == computerChoice:
         print("It's a tie!")
     elif playerChoice == 1 and computerChoice == 3:
@@ -21,6 +23,6 @@ else:
     elif playerChoice == 2 and computerChoice == 1:
         print("Paper beats Rock - You win!")
     elif playerChoice == 3 and computerChoice == 2:
-        print("Scissors beats Paper - You win!")
+        print("Scissors beats Paper - You win!")     
     else:
-        print("You lose!")                    
+        print("You lose!")   
